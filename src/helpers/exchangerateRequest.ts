0955d07de80eb46f151649304b1abab53e5445b4
Eliut González <reliutg@gmail.com>
2021-08-19 15:55:25 +0200

    chore: formatting and linting

--- src/helpers/exchangerateRequest.ts
+++ src/helpers/exchangerateRequest.ts
@@ -1,15 +1,15 @@
-import axios, { AxiosRequestConfig, Method } from "axios";
-import * as url from "url";
+import axios, { AxiosRequestConfig, Method } from 'axios';
+import * as url from 'url';
 
 export class ExchangerateRequest {
   // serialize javascript object to query string
   serialize(object: any) {
     return new URLSearchParams(object).toString();
   }
-  prepareRequest(url: string, method: Method | undefined, query: string) {
+  prepareRequest(uri: string, method: Method | undefined, query: string) {
     return {
-      url: `${url}?${query}`,
-      method: method,
+      url: `${uri}?${query}`,
+      method,
     };
   }
   sendRequest(request: AxiosRequestConfig) {
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-import { ExchangerateRequest } from "./helpers/exchangerateRequest";
+import { ExchangerateRequest } from './helpers/exchangerateRequest';
 
 interface ExchangerateRequestParams {
   from?: string;
   to?: string;
   date?: string;
-  symbols?: Array<string>,
-  amount?: number,
-  places?: number,
-  format?: string,
-  source?: string,
-  callback?: Function
+  /* tslint:disable */
+  symbols?: Array<string>;
+  amount?: number;
+  places?: number;
+  format?: string;
+  source?: string;
+  callback?: () => void;
 }
 
 export class Exchangerate extends ExchangerateRequest {
-  requestURL = "https://api.exchangerate.host";
+  requestURL = 'https://api.exchangerate.host';
 
   /**
    * Get the latest foreign exchange reference rates.
@@ -31,11 +32,7 @@ export class Exchangerate extends ExchangerateRequest {
       // serialize the parameters
       const query = this.serialize(parameters);
       // prepare the request
-      const request = this.prepareRequest(
-        `${this.requestURL}/latest`,
-        "GET",
-        query
-      );
+      const request = this.prepareRequest(`${this.requestURL}/latest`, 'GET', query);
       // send the request
       return this.sendRequest(request);
     } catch (error) {
@@ -60,20 +57,16 @@ export class Exchangerate extends ExchangerateRequest {
     try {
       // from and to are required
       if (!parameters.from || !parameters.to) {
-        throw new Error("from and to are required");
+        throw new Error('from and to are required');
       }
       // date format must be YYYY-MM-DD
       if (parameters.date && !/^\d{4}-\d{2}-\d{2}$/.test(parameters.date)) {
-        throw new Error("date must be in YYYY-MM-DD format");
+        throw new Error('date must be in YYYY-MM-DD format');
       }
       // serialize the parameters
       const query = this.serialize(parameters);
       // prepare the request
-      const request = this.prepareRequest(
-        `${this.requestURL}/convert`,
-        "GET",
-        query
-      );
+      const request = this.prepareRequest(`${this.requestURL}/convert`, 'GET', query);
       // send the request
       return this.sendRequest(request);
     } catch (error) {
@@ -81,31 +74,27 @@ export class Exchangerate extends ExchangerateRequest {
     }
   }
   /**
-  * Historical rates are available for most currencies all the way back to the year of 1999. 
-  * @param {string} date - Date of the exchange rate you would like to get.
-  * @param {object} parameters - Parameters for the request
-  * @param {string} parameters.base - Base currency
-  * @param {Array} parameters.symbols - Symbols to convert
-  * @param {number} parameters.amount - Amount to convert
-  * @param {function} parameters.callback - Callback function
-  * @param {number} parameters.places - Number of decimal places
-  * @param {string} parameters.format - Format of the response
-  * @param {string} parameters.source - Source of the exchange rate
-  */
+   * Historical rates are available for most currencies all the way back to the year of 1999.
+   * @param {string} date - Date of the exchange rate you would like to get.
+   * @param {object} parameters - Parameters for the request
+   * @param {string} parameters.base - Base currency
+   * @param {Array} parameters.symbols - Symbols to convert
+   * @param {number} parameters.amount - Amount to convert
+   * @param {function} parameters.callback - Callback function
+   * @param {number} parameters.places - Number of decimal places
+   * @param {string} parameters.format - Format of the response
+   * @param {string} parameters.source - Source of the exchange rate
+   */
   async historicalRates(date: string, parameters?: Omit<ExchangerateRequestParams, 'from' | 'to'>): Promise<any> {
     try {
       // date format must be YYYY-MM-DD
       if (date && !/^\d{4}-\d{2}-\d{2}$/.test(date)) {
-        throw new Error("date must be in YYYY-MM-DD format");
+        throw new Error('date must be in YYYY-MM-DD format');
       }
       // serialize the parameters
       const query = this.serialize(parameters);
       // prepare the request
-      const request = this.prepareRequest(
-        `${this.requestURL}/${date}`,
-        "GET",
-        query
-      );
+      const request = this.prepareRequest(`${this.requestURL}/${date}`, 'GET', query);
       // send the request
       return this.sendRequest(request);
     } catch (error) {
@@ -113,39 +102,39 @@ export class Exchangerate extends ExchangerateRequest {
     }
   }
   /**
-  * Timeseries endpoint are for daily historical rates between two dates of your choice, with a maximum time frame of 366 days.
-  * @param {string} startDate - The start date of your preferred timeframe.
-  * @param {string} endDate - The end date of your preferred timeframe.
-  * @param {object} parameters - Parameters for the request
-  * @param {string} parameters.base - Base currency
-  * @param {Array} parameters.symbols - Symbols to convert
-  * @param {number} parameters.amount - Amount to convert
-  * @param {function} parameters.callback - Callback function
-  * @param {number} parameters.places - Number of decimal places
-  * @param {string} parameters.format - Format of the response
-  * @param {string} parameters.source - Source of the exchange rate
-  */
-  async timeseries(startDate: string, endDate: string, parameters?: Omit<ExchangerateRequestParams, 'from' | 'to'>): Promise<any> {
+   * Timeseries endpoint are for daily historical rates between two dates of your choice, with a maximum time frame of 366 days.
+   * @param {string} startDate - The start date of your preferred timeframe.
+   * @param {string} endDate - The end date of your preferred timeframe.
+   * @param {object} parameters - Parameters for the request
+   * @param {string} parameters.base - Base currency
+   * @param {Array} parameters.symbols - Symbols to convert
+   * @param {number} parameters.amount - Amount to convert
+   * @param {function} parameters.callback - Callback function
+   * @param {number} parameters.places - Number of decimal places
+   * @param {string} parameters.format - Format of the response
+   * @param {string} parameters.source - Source of the exchange rate
+   */
+  async timeseries(
+    startDate: string,
+    endDate: string,
+    parameters?: Omit<ExchangerateRequestParams, 'from' | 'to'>,
+  ): Promise<any> {
     try {
       // startDate and endDate format must be YYYY-MM-DD
       if (startDate && !/^\d{4}-\d{2}-\d{2}$/.test(startDate)) {
-        throw new Error("startDate must be in YYYY-MM-DD format");
+        throw new Error('startDate must be in YYYY-MM-DD format');
       }
       if (endDate && !/^\d{4}-\d{2}-\d{2}$/.test(endDate)) {
-        throw new Error("endDate must be in YYYY-MM-DD format");
+        throw new Error('endDate must be in YYYY-MM-DD format');
       }
       // endDate must be after startDate
       if (endDate && startDate && new Date(endDate) < new Date(startDate)) {
-        throw new Error("endDate must be after startDate");
+        throw new Error('endDate must be after startDate');
       }
       // serialize the parameters
       const query = this.serialize({ ...parameters, start_date: startDate, end_date: endDate });
       // prepare the request
-      const request = this.prepareRequest(
-        `${this.requestURL}/timeseries`,
-        "GET",
-        query
-      );
+      const request = this.prepareRequest(`${this.requestURL}/timeseries`, 'GET', query);
       // send the request
       return this.sendRequest(request);
     } catch (error) {
@@ -153,39 +142,39 @@ export class Exchangerate extends ExchangerateRequest {
     }
   }
   /**
-  * Using the fluctuation endpoint you will be able to retrieve information about how currencies fluctuate on a day-to-day basis.
-  * @param {string} startDate - The start date of your preferred timeframe.
-  * @param {string} endDate - The end date of your preferred timeframe.
-  * @param {object} parameters - Parameters for the request
-  * @param {string} parameters.base - Base currency
-  * @param {Array} parameters.symbols - Symbols to convert
-  * @param {number} parameters.amount - Amount to convert
-  * @param {function} parameters.callback - Callback function
-  * @param {number} parameters.places - Number of decimal places
-  * @param {string} parameters.format - Format of the response
-  * @param {string} parameters.source - Source of the exchange rate
-  */
-  async fluctuation(startDate: string, endDate: string, parameters?: Omit<ExchangerateRequestParams, 'from' | 'to'>): Promise<any> {
+   * Using the fluctuation endpoint you will be able to retrieve information about how currencies fluctuate on a day-to-day basis.
+   * @param {string} startDate - The start date of your preferred timeframe.
+   * @param {string} endDate - The end date of your preferred timeframe.
+   * @param {object} parameters - Parameters for the request
+   * @param {string} parameters.base - Base currency
+   * @param {Array} parameters.symbols - Symbols to convert
+   * @param {number} parameters.amount - Amount to convert
+   * @param {function} parameters.callback - Callback function
+   * @param {number} parameters.places - Number of decimal places
+   * @param {string} parameters.format - Format of the response
+   * @param {string} parameters.source - Source of the exchange rate
+   */
+  async fluctuation(
+    startDate: string,
+    endDate: string,
+    parameters?: Omit<ExchangerateRequestParams, 'from' | 'to'>,
+  ): Promise<any> {
     try {
       // startDate and endDate format must be YYYY-MM-DD
       if (startDate && !/^\d{4}-\d{2}-\d{2}$/.test(startDate)) {
-        throw new Error("startDate must be in YYYY-MM-DD format");
+        throw new Error('startDate must be in YYYY-MM-DD format');
       }
       if (endDate && !/^\d{4}-\d{2}-\d{2}$/.test(endDate)) {
-        throw new Error("endDate must be in YYYY-MM-DD format");
+        throw new Error('endDate must be in YYYY-MM-DD format');
       }
       // endDate must be after startDate
       if (endDate && startDate && new Date(endDate) < new Date(startDate)) {
-        throw new Error("endDate must be after startDate");
+        throw new Error('endDate must be after startDate');
       }
       // serialize the parameters
       const query = this.serialize({ ...parameters, start_date: startDate, end_date: endDate });
       // prepare the request
-      const request = this.prepareRequest(
-        `${this.requestURL}/fluctuation`,
-        "GET",
-        query
-      );
+      const request = this.prepareRequest(`${this.requestURL}/fluctuation`, 'GET', query);
       // send the request
       return this.sendRequest(request);
     } catch (error) {
@@ -193,21 +182,17 @@ export class Exchangerate extends ExchangerateRequest {
     }
   }
   /**
-  * API comes with a constantly updated endpoint returning all available currencies.
-  * @param {object} parameters - Parameters for the request
-  * @param {function} parameters.callback - Callback function
-  * @param {string} parameters.format - Format of the response
-  */
+   * API comes with a constantly updated endpoint returning all available currencies.
+   * @param {object} parameters - Parameters for the request
+   * @param {function} parameters.callback - Callback function
+   * @param {string} parameters.format - Format of the response
+   */
   async symbols(parameters?: Pick<ExchangerateRequestParams, 'callback' | 'format'>): Promise<any> {
     try {
       // serialize the parameters
       const query = this.serialize(parameters);
       // prepare the request
-      const request = this.prepareRequest(
-        `${this.requestURL}/symbols`,
-        "GET",
-        query
-      );
+      const request = this.prepareRequest(`${this.requestURL}/symbols`, 'GET', query);
       // send the request
       return this.sendRequest(request);
     } catch (error) {
@@ -215,22 +200,18 @@ export class Exchangerate extends ExchangerateRequest {
     }
   }
   /**
-  * Our accurate EU VAT information API simplifies in and around the European Union. 
-  * @param {object} parameters - Parameters for the request
-  * @param {Array} parameters.symbols - Symbols to convert
-  * @param {function} parameters.callback - Callback function
-  * @param {string} parameters.format - Format of the response
-  */
+   * Our accurate EU VAT information API simplifies in and around the European Union.
+   * @param {object} parameters - Parameters for the request
+   * @param {Array} parameters.symbols - Symbols to convert
+   * @param {function} parameters.callback - Callback function
+   * @param {string} parameters.format - Format of the response
+   */
   async EUVATRates(parameters?: Pick<ExchangerateRequestParams, 'symbols' | 'callback' | 'format'>): Promise<any> {
     try {
       // serialize the parameters
       const query = this.serialize(parameters);
       // prepare the request
-      const request = this.prepareRequest(
-        `${this.requestURL}/vat_rates`,
-        "GET",
-        query
-      );
+      const request = this.prepareRequest(`${this.requestURL}/vat_rates`, 'GET', query);
       // send the request
       return this.sendRequest(request);
     } catch (error) {
